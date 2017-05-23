e137d58d99cebae52fc6903e79fe4924dcc1ed04
cvasquez87 <crizvasqz2015@gmail.com>
2017-05-24 05:27:35 +0800

    Update base.js

--- src/config/base.js
+++ src/config/base.js
@@ -2,7 +2,7 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-	ACCOUNT_KEY: '',
+	ACCOUNT_KEY: '16e14v4WqZxe5Gdt0TUuLh4OuU6Wp4rd',
 	// Set to 'docked' or 'normal' for docked or normal mode respectively
 	THEME: 'normal'
 }
